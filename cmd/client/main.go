c112cd7e7034a2dfc5a6cd2dd50086c6db8f555e
Sidharth Arun <sidarun88@gmail.com>
2025-10-03 12:48:57 +0530

    Declare a simple transient queue to listen to published messages

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,55 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+	"github.com/sidarun88/learn-pub-sub-starter/internal/gamelogic"
+	"github.com/sidarun88/learn-pub-sub-starter/internal/pubsub"
+	"github.com/sidarun88/learn-pub-sub-starter/internal/routing"
+)
+
+const amqpURI = "amqp://guest:guest@localhost:5672/"
 
 func main() {
 	fmt.Println("Starting Peril client...")
+
+	conn, err := amqp.Dial(amqpURI)
+	if err != nil {
+		log.Fatalf("Failed to connect to RabbitMQ: %s", err)
+	}
+
+	defer conn.Close()
+	fmt.Println("Connected to RabbitMQ")
+
+	username, err := gamelogic.ClientWelcome()
+	if err != nil {
+		log.Fatalf("Failed to get username: %s", err)
+	}
+
+	queueName := routing.PauseKey + "." + username
+	ch, queue, err := pubsub.DeclareAndBind(
+		conn,
+		routing.ExchangePerilDirect,
+		queueName,
+		routing.PauseKey,
+		pubsub.TransientQueue,
+	)
+	if err != nil {
+		log.Fatalf("Failed to declare queue: %s", err)
+	}
+
+	defer ch.Close()
+	fmt.Printf("Declared and bounded queue '%s'\n", queue.Name)
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
+	fmt.Println("Program running. Press Ctrl+C to exit gracefully.")
+
+	<-signalChan
+	fmt.Println("Exiting...")
 }
